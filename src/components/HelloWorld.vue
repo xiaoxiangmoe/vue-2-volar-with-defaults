36563e54f19df3788c9b93504aee4fb7b298567d
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2022-01-17 17:42:11 +0800

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    msg: string;
+  }>(),
+  {
+    msg: "ok",
+  }
+);
+</script>
+
+<template>
+  <div>{{ msg }}, {{ msg.length }}</div>
+</template>
